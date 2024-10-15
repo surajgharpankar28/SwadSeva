d103015c333251f60a78dcc37024d0f35887d87c
Suraj Gharpankar <surajgharpankar28@gmail.com>
2024-10-15 13:10:45 +0530

    Added RestroCard and Shimmer

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 import Header from "./components/Header";
+import Body from "./components/Body";
 const AppLayout = () => {
   return (
     <div className="App">
       <Header />
+      <Outlet />
     </div>
   );
 };
@@ -15,28 +17,28 @@ const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
-    // children: [
-    //   {
-    //     path: "/",
-    //     element: <Body />,
-    //   },
-    //   {
-    //     path: "/about",
-    //     element: (
-    //       <Suspense fallback={<h1>Loading...</h1>}>
-    //         <About />
-    //       </Suspense>
-    //     ),
-    //   },
-    //   {
-    //     path: "/contact",
-    //     element: <Contact />,
-    //   },
-    //   {
-    //     path: "/restaurants/:resId",
-    //     element: <RestroMenu />,
-    //   },
-    // ],
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      //   {
+      //     path: "/about",
+      //     element: (
+      //       <Suspense fallback={<h1>Loading...</h1>}>
+      //         <About />
+      //       </Suspense>
+      //     ),
+      //   },
+      //   {
+      //     path: "/contact",
+      //     element: <Contact />,
+      //   },
+      //   {
+      //     path: "/restaurants/:resId",
+      //     element: <RestroMenu />,
+      //   },
+    ],
     // errorElement: <Error />,
   },
 ]);
---
+++ src/components/RestroCard.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { CON_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
+
+const RestroCard = (props) => {
+  const { resData } = props;
+
+  const { id, name, cuisines, avgRating, cloudinaryImageId, deliveryTime } =
+    resData?.info;
+
+  return (
+    <div className="res-card">
+      <div>
+        <img
+          className="res-img"
+          alt="res-img"
+          src={CON_URL + cloudinaryImageId}
+        />
+      </div>
+      <div>
+        <h3>{name}</h3>
+        <h5>{cuisines.join(", ")}</h5>
+        <h5>{avgRating} stars</h5>
+        <h5>ETA : {deliveryTime} mins</h5>
+        <button>
+          <Link to={"/restaurants/" + id}>View Menu</Link>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default RestroCard;
