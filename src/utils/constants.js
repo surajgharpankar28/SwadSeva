d3ea3a4fa4247a87352713ca83f52e36a0d53ebd
Suraj Gharpankar <surajgharpankar28@gmail.com>
2024-10-27 23:53:24 +0530

    -

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,7 +7,13 @@ export const CURATED_FOOD_TYPE = `${process.env.PARCEL_MEDIA_ASSETS_BASE_URL}ima
 export const CON_URL = `${process.env.PARCEL_MEDIA_ASSETS_BASE_URL}image/upload/fl_lossy,f_auto,q_auto,w_660/`;
 
 // Swiggy API to get Restaurant data using swadseva server
-export const SWADSEVA_API_URL = `${process.env.PARCEL_FOODFIRE_APP_BASE_URL}restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING`;
+export const SWADSEVA_API_URL = `${process.env.PARCEL_SWADSEVA_APP_BASE_URL}restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING`;
 
 // Swiggy API to get Restaurant Menu data using swadseva server
-export const MENU_API = `${process.env.PARCEL_FOODFIRE_APP_BASE_URL}menu?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId=`;
+export const MENU_API = `${process.env.PARCEL_SWADSEVA_APP_BASE_URL}menu?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId=`;
+
+// shimmer card unit
+export const shimmer_card_unit = 20;
+
+// shimmer Menu card unit
+export const shimmer_menu_card_unit = 10;
